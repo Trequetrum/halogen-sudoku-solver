e431febb494db450df0b5742db684e1d47959d2f
Trequetrum <63251124+Trequetrum@users.noreply.github.com>
2021-05-14 15:26:46 -0400

    OSet Comment Overhaul (Abstracted Away from Sudoku Cells)

--- src/sudoku/OSet.purs
+++ src/sudoku/OSet.purs
@@ -1,5 +1,8 @@
--- | An OSet is a set of options. It encodes the options that are still
--- | possible at a given position in a Sudoku Board. 
+-- | An OSet is a set of options. 
+-- |
+-- | One use of an OSet is to encode the options that are still possible at a given 
+-- | position in a Sudoku Board. Some strategies encode information about their
+-- | search-space as well.
 -- |
 module Sudoku.OSet 
   ( module SetType
@@ -48,12 +51,11 @@ import Sudoku.Option (Option, allOptions, asString, indexOf, invalidOption, numO
 -- Smart Constructors for Cells
 -------------------------------------------------------------------
 
--- | The OSet where all options are still possible
+-- | Every option is an element of this set
 allOptionsSet :: OSet
 allOptionsSet = OSet allOptionsInt
 
--- | The cell where no options are still possible. This is not a valid 
--- | cell in a sudoku board, but it is still a valid combination of options.
+-- | The empty set
 noOptionsSet :: OSet
 noOptionsSet = OSet 0
 
@@ -63,8 +65,7 @@ noOptionsSet = OSet 0
 allSets :: List OSet
 allSets = range 1 numOfOptions >>= (setsOfSize >>> fromFoldable)
 
--- | Return all the combinations of possible sets that have exactly 
--- | N options remaining
+-- | Return all the sets with the cardinality equal to the input
 setsOfSize :: Int -> Array OSet
 setsOfSize n = filter (countOptions >>> (_ == n)) $ coerce $ 1 .. allOptionsInt
 
@@ -72,53 +73,50 @@ setsOfSize n = filter (countOptions >>> (_ == n)) $ coerce $ 1 .. allOptionsInt
 -- OSet Operations
 -------------------------------------------------------------------
 
--- | Encode an Option as a set with one possible option 
+-- | Encode an Option as a set with cardinality 1
 toOSet :: Option -> OSet
 toOSet n = OSet $ 1 `Bi.shl` indexOf n
 
--- | Return a set where the given option is guarenteed to be one of the possible
--- | options remaining
+-- | Return a set where the given option is guarenteed to be an element of the set
 setOption :: Option -> OSet -> OSet
 setOption = toOSet >>> setOptions
 
--- | Return a set where every possible option in the first set is guarenteed 
--- | to be one of the possible options remaining
+-- | Return the union of two sets
 setOptions :: OSet -> OSet -> OSet
 setOptions = (.|.)
 
--- | Return a set where the given option is guarenteed not to be one of the possible
--- | options remaining
+-- | Return a set where the given option is guarenteed not to be an element of the set
 dropOption :: Option -> OSet -> OSet
 dropOption = toOSet >>> dropOptions
 
--- | Return a set where every possible option in the first set is guarenteed 
--- | not to be one of the possible options remaining
+-- | Return a set where every element of the first set is guarenteed not to be an 
+-- | element of the returned set
 dropOptions :: OSet -> OSet -> OSet
 dropOptions options set = bNot options .&. set
 
--- | Return a set where the given option is guarenteed to be different
+-- | Return a set where Option is either removed from or added as an element such
 toggleOption :: Option -> OSet -> OSet
 toggleOption = toOSet >>> toggleOptions
 
--- | Return a set where every possible option in the first set is guarenteed 
--- | to be different if it's in the second set
+-- | Pairwise XOR (this is the union of differences)
+-- | Written as: ( (A - B) U (B - A) )
 toggleOptions :: OSet -> OSet -> OSet
 toggleOptions = (.^.)
 
--- | Return a set where every Option is different (It's complement)
+-- | Return the complement of the set.
 toggleOSet :: OSet -> OSet
 toggleOSet = bNot
 
--- | return an array of Options where each Option is an element of the set
+-- | return an array of options where each option is an element of the set
 asOptions :: OSet -> Array Option
 asOptions set = filter (flip hasOption $ set) allOptions
 
--- | Try to return the first possible option of a cell
+-- | Try to return the first possible option of a set.
 firstOption :: OSet -> Maybe Option
 firstOption set = find (flip hasOption set) allOptions
 
 -- | Like firstOption, but might return invalidOption
--- | Generally best used of countOptions was used first
+-- | Generally best used if countOptions was used first
 trustFirstOption :: OSet -> Option
 trustFirstOption = firstOption >>> fromMaybe invalidOption
 
@@ -127,10 +125,10 @@ countOptions :: OSet -> Int
 countOptions set = fromMaybe 0 $ countOptionsLookupTable !! (coerce set - 1)
 
 -------------------------------------------------------------------
--- Predicates for Cells
+-- Predicates for OSets
 -------------------------------------------------------------------
 
--- | Check if a set is valid
+-- | Check if a set is valid as a cell
 -- |   * It has elements (not the empty set)
 -- |   * All its elements are options
 isValid :: OSet -> Boolean
@@ -139,7 +137,7 @@ isValid set = n > 0 && n <= allOptionsInt
     n :: Int
     n = coerce set
 
--- | Check if a set has an option
+-- | Check if an option is an element of a set
 hasOption :: Option -> OSet -> Boolean
 hasOption = toOSet >>> notDisjoint
 
@@ -148,10 +146,12 @@ notDisjoint :: OSet -> OSet -> Boolean
 notDisjoint a b = a .&. b /= OSet 0
 
 -- | Check if the first set is a superset of the second set
+-- | This is not strict, so (a `isSuperset` a) == true
 isSuperset :: OSet -> OSet -> Boolean
 isSuperset a b = a .|. b == a
 
 -- | Check if the first set is a subset of the second set
+-- | This is not strict, so (a `isSubset` a) == true
 isSubset :: OSet -> OSet -> Boolean
 isSubset = flip isSuperset
 
